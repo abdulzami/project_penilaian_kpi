c03100e2fff5e0b34b97fd6345a3ad80af961df6
Abdul Aziz Zam Zami <zamibojez@gmail.com>
2021-11-15 18:29:48 +0700

    Tampilan hasil penilaian di home user dinilai
    Berisi total nilai, catatan penting dan pengurangan nilai

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Jadwal;
 use App\Models\Penilai;
+use App\Models\Penilaian;
+use App\Models\PenilaianPerformance;
+use App\Models\PenilaianPerilaku;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        
+
         if (!Auth::check()) {
             return redirect('/');
         }
@@ -20,22 +24,43 @@ class HomeController extends Controller
         $sekarang = Carbon::now();
         $sekarang = $sekarang->toDateString();
         $jadwal = Jadwal::whereRaw('? between tanggal_mulai and tanggal_akhir', $sekarang)->get();
-        if($jadwal->isEmpty())
-        {
+        if ($jadwal->isEmpty()) {
             $nama_periode = "";
-        }else{
+        } else {
             $nama_periode = $jadwal[0]->nama_periode;
         }
         // $id = Auth::user()->id_user;
-        
+
         if (Auth::user()->level == 'admin') {
-            return view('admin.home',compact('nama_periode'));
-        } 
-        elseif(Auth::user()->level == 'pegawai')
-        {
-            return view('pegawai.home',compact('nama_periode'));
-        }
-        else {
+            return view('admin.home', compact('nama_periode'));
+        } elseif (Auth::user()->level == 'pegawai') {
+            $penilaian = Penilaian::where('id_pegawai', Auth::user()->id_user)->where('id_jadwal', $jadwal[0]->id_jadwal)->get();
+            for ($i = 0; $i < sizeof($penilaian); $i++) {
+                $performances = PenilaianPerformance::select(DB::raw("CASE
+                    WHEN tipe_performance = 'min' AND target>realisasi THEN 100
+                    WHEN tipe_performance = 'min' THEN ((target/realisasi)*100)*bobot/100
+                    WHEN tipe_performance = 'max' THEN ((realisasi/target) * 100)*bobot/100
+                    END AS skor"))
+                    ->join('kpi_performances', 'kpi_performances.id_performance', '=', 'penilaian_performances.id_performance')
+                    ->where('id_penilaian', $penilaian[$i]->id_penilaian)->get();
+                $performances = $performances->sum('skor');
+                $performances = $performances * 70 / 100;
+                $perilakus = PenilaianPerilaku::select(DB::raw('SUM(nilai_perilaku *20 * 100/6/100)*30/100 AS skor_akhir'))->where('id_penilaian', $penilaian[$i]->id_penilaian)->get();
+                $perilakus = (float)$perilakus[0]->skor_akhir;
+                $total = round(($performances + $perilakus), 5);
+                $penilaian[$i]->performance = $performances;
+                $penilaian[$i]->perilaku = $perilakus;
+                $penilaian[$i]->total = $total;
+                $penilaian[$i]->capaian = $total . " %";
+            }
+            if ($penilaian->isEmpty()) {
+                return view('pegawai.home', compact('nama_periode'));
+            } else {
+                $penilaian = $penilaian[0];
+                $kpiperformances = PenilaianPerformance::leftjoin('kpi_performances', 'kpi_performances.id_performance', '=', 'penilaian_performances.id_performance')->where('id_penilaian', $penilaian->id_penilaian)->get();
+                return view('pegawai.home', compact('nama_periode', 'penilaian', 'kpiperformances'));
+            }
+        } else {
             return redirect('/')->with('error', 'Anda tidak punya akses !');
         }
     }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -40,33 +40,33 @@
                 </li>
             @endif
             @if (Auth::user()->level == 'pegawai')
-            @penilai
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-notepad"></i>
-                    <span class="nav-text">Penilaian</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{route('belum-dinilai')}}">Belum Dinilai</a></li>
-                    <li><a href="{{route('menunggu-verifikasi')}}">Menunggu Verifikasi</a></li>
-                    <li><a href="#">Banding Penilaian</a></li>
-                    <li><a href="#">Selesai</a></li>
-                </ul>
-            </li>
-            {{-- <li>
+                @penilai
+                <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                        <i class="flaticon-381-notepad"></i>
+                        <span class="nav-text">Penilaian</span>
+                    </a>
+                    <ul aria-expanded="false">
+                        <li><a href="{{ route('belum-dinilai') }}">Belum Dinilai</a></li>
+                        <li><a href="{{ route('menunggu-verifikasi') }}">Menunggu Verifikasi</a></li>
+                        <li><a href="#">Banding Penilaian</a></li>
+                        <li><a href="#">Selesai</a></li>
+                    </ul>
+                </li>
+                {{-- <li>
                 <a href="javascript:void()" class="ai-icon" aria-expanded="true">
                     <i class="fa fa-history" style="font-size: 24px"></i>
                     <span class="nav-text">Histori Banding</span>
                 </a>
             </li> --}}
-            @endpenilai
-            @atasanpenilai
-            <li>
-                <a href="{{route('approve-penilaian')}}" class="ai-icon" aria-expanded="true">
-                    <i class="flaticon-381-list-1" style="font-size: 24px"></i>
-                    <span class="nav-text">Approve Penilaian</span>
-                </a>
-            </li>
-            @endatasanpenilai
+                @endpenilai
+                @atasanpenilai
+                <li>
+                    <a href="{{ route('approve-penilaian') }}" class="ai-icon" aria-expanded="true">
+                        <i class="flaticon-381-list-1" style="font-size: 24px"></i>
+                        <span class="nav-text">Approve Penilaian</span>
+                    </a>
+                </li>
+                @endatasanpenilai
             @endif
         </ul>
     </div>
