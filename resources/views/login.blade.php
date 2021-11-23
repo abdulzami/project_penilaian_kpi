f67277592b7cdaabf246ba8f3578efb015f34b98
D'Kerjain <50320881+shindipurnama@users.noreply.github.com>
2021-11-23 18:34:52 +0700

    logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>{{ $judul }} - KPI - PT Barata Indonesia Persero</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/kpi.png') }}">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
 </head>
